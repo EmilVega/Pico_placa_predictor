7717e066f08cf82633bb1a11d8cabfcb5f49f8d4
Emil <emil.vega08@gmail.com>
2019-10-05 23:14:21 -0500

    Create constrint script for the prediction

---
+++ ConstraintValues.m
@@ -0,0 +1,14 @@
+% Define the constrainst values, time and days.
+% container.Map allows to have objects that maps values to unique keys
+
+function [timeConstraint, daysConstraint] = ConstraintValues()
+
+    keySet1 = {'Morning','Night'};
+    valueSet1 = {["7:30","9:30"], ["16:00","19:30"]};
+    timeConstraint = containers.Map(keySet1,valueSet1,'UniformValues',false);
+
+    keySet2 = {'Monday','Tuesday','Wednesday','Thursday','Friday'};
+    valueSet2 = {[1 2],[3 4],[5 6],[7 8],[9 0]};
+    daysConstraint = containers.Map(keySet2,valueSet2,'UniformValues',false);
+    
+end
